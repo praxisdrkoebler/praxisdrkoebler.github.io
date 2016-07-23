0efeab464caea39000ed0fa7b4c7d12c30685ebd
Niko Köbler <niko@n-k.de>
2016-07-23 19:40:49 +0200

    better navbar behaviour

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-$(document).ready(function() {
-  $(".dropdown-button").dropdown({hover: false});
-  $(".button-collapse").sideNav();
-  $('.slider').slider({
-    full_width: true,
-    indicators: false,
-    transition: 1500,
-    height: 600
-  });
+$(document).ready(function () {
+    $('.dropdown-button').dropdown({
+        hover: true,
+        belowOrigin: true,
+        inDuration: 500
+    });
+    $('.button-collapse').sideNav();
+    $('.slider').slider({
+        full_width: true,
+        indicators: false,
+        transition: 1500,
+        height: 500
+    });
+
+    $(window).scroll(handleScroll);
 });
+
+var handleScroll = function() {
+    if ((window.scrollY || window.pageYOffset) > 500) {
+        $('.navbar').addClass('navbar-fixed').css({top: '0px'});
+    } else {
+        $('.navbar').removeClass('navbar-fixed').css({top: '458px'});
+    }
+};
