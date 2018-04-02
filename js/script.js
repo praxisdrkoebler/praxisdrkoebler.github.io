436023ffc7b5ad3e9c60c0f562481d91b91e5e2a
Niko Köbler <niko@n-k.de>
2018-04-02 15:14:12 +0200

    temp page

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-var navOffset = 256;
-
-$(document).ready(function () {
-    $('.dropdown-button').dropdown({
-        hover: true,
-        belowOrigin: true,
-        inDuration: 500
-    });
-    $('.button-collapse').sideNav();
-    $('.slider').slider({
-        full_width: true,
-        indicators: false,
-        transition: 1500,
-        height: 500
-    });
-
-    if ($('.navbar.index').length) {
-      navOffset = 458;
-    }
-    $(window).scroll(handleScroll);
+$(document).ready(function() {
+  $('.parallax').parallax();
 });
-
-var handleScroll = function() {
-    if ((window.scrollY || window.pageYOffset) >= navOffset) {
-        $('.navbar').addClass('navbar-fixed').css({top: '0px'});
-    } else {
-        $('.navbar').removeClass('navbar-fixed').css({top: navOffset + 'px'});
-    }
-};
