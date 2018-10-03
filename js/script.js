d5a41bfad998d38a61968f0c7ecb9ade586f318b
Niko Köbler <niko@n-k.de>
2018-10-03 17:56:52 +0200

    update ressources

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-$(document).ready(function() {
-  $('.parallax').parallax();
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
