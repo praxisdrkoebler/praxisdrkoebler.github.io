222768be0ab4e8893f48e6a2c6101ba8c78c9d55
Niko Köbler <niko@n-k.de>
2018-10-06 17:05:27 +0200

    some more changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
   M.Sidenav.init(document.querySelectorAll('.sidenav'));
   M.Parallax.init(document.querySelectorAll('.parallax'));
-  M.Collapsible.init(document.querySelectorAll('.collapsible'), {accordion: false});
+  M.Collapsible.init(document.querySelectorAll('.collapsible'));
+  M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+});
+
+var navScrollOffset = window.outerWidth <= 400 ? 220 : window.outerWidth <= 1024 ? 420 : 530;
+var navTransparent = false;
+document.addEventListener('scroll', function() {
+  if (!navTransparent && window.scrollY > navScrollOffset) {
+    document.querySelector('nav').className = 'orchid-transparent';
+    navTransparent = !navTransparent;
+  } else if (navTransparent && window.scrollY <= navScrollOffset) {
+    document.querySelector('nav').className = 'orchid';
+    navTransparent = !navTransparent;
+  }
 });
